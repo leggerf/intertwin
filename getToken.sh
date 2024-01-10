0513821a590a716bf2aa001060bb5db762111f53
Federica Legger <federica.legger@cern.ch>
2024-01-10 17:20:01 +0100

    get token without passwd

--- getToken.sh
+++ getToken.sh
@@ -7,22 +7,31 @@
 ### - sets the environment to access the endpoint at CNAF with gfal
 ### - sets the environment for the intertwin rucio instance at DESY
 
+## Get token from EGI
+
 eval `oidc-agent`
 
-#token obtained with
-# oidc-gen --pub --issuer https://aai-demo.egi.eu/auth/realms/egi --scope=max  --flow=device egi
+export OIDC_ENCRYPTION_PW="intertwin"
 
-#oidc-token legger
+# Identity egi added with:
+# oidc-gen --pub --issuer https://aai-demo.egi.eu/auth/realms/egi --scope=max  --flow=device egi
+# Use encryption password "intertwin" !!!!
+
+oidc-add egi --pw-env 
+
 export BEARER_TOKEN="$(oidc-token egi --time 3600 --scope 'openid profile offline_access eduperson_entitlement')"
 export EXPIRES_AT="$(date -d @$(oidc-token egi --expires-at))"
 
-#for rucio
+# SETUP for Rucio
 
 export RUCIO_CONFIG="/afs/cern.ch/user/l/leggerf/intertwin/rucio.cfg"
+
 echo $BEARER_TOKEN | tr -cd '[:print:]' > /tmp/rucio.tkn
 cat /tmp/rucio.tkn
 
 printf "\n\nExpires at $EXPIRES_AT"
+
+# Check token is working with gfal commands
 
 printf "\n\ngfal-ls davs://xfer-intertwin.cr.cnaf.infn.it:8443/intertwin/virgo \n"
 gfal-ls davs://xfer-intertwin.cr.cnaf.infn.it:8443/intertwin/virgo
